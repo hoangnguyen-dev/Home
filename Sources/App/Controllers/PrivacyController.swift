cab022ed63c601ff3684484d906b5c04b4747026
hoangnguyen <nvhoang9900@gmail.com>
2021-11-23 19:21:28 +0700

    nothing
    add terms and privacy

---
+++ Sources/App/Controllers/PrivacyController.swift
@@ -0,0 +1,51 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hoang Nguyen on 23/11/2021.
+//
+
+import Foundation
+import Vapor
+import Leaf
+
+class PrivacyController {
+//    func terms(_ req: Request) throws -> EventLoopFuture<View> {
+//        let context = [String: String]()
+//        return req.view.render("terms", context)
+//    }
+//
+//    func privacy(_ req: Request) throws  -> EventLoopFuture<View>  {
+//        let context = [String: String]()
+//        return req.view.render("privacy", context)
+//    }
+//
+    // MARK: Symbols
+    func symbolsPrivacy(_ req: Request) throws -> EventLoopFuture<View> {
+        let context = [String: String]()
+        return req.view.render("symbols_privacy", context)
+    }
+    
+    // MARK: Quotes
+    func quotesTerms(_ req: Request) throws -> EventLoopFuture<View> {
+        let context = [String: String]()
+        return req.view.render("quotes_terms", context)
+    }
+    
+    func quotesPrivacy(_ req: Request) throws -> EventLoopFuture<View> {
+        let context = [String: String]()
+        return req.view.render("quotes_privacy", context)
+    }
+    
+    // MARK: Caro Online
+    func caroOnlinePrivacy(_ req: Request) throws -> EventLoopFuture<View> {
+        let context = [String: String]()
+        return req.view.render("caro_privacy", context)
+    }
+    
+    // MARK: Keyboard
+    func keyboardPrivacy(_ req: Request) throws -> EventLoopFuture<View> {
+        let context = [String: String]()
+        return req.view.render("keyboard_privacy", context)
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,18 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    let controller = PrivacyController()
+    //app.get("terms", use: controller.terms)
+    //app.get("privacy", use: controller.privacy)
+    
+    // Symbols
+    app.get("symbols", "privacy", use: controller.symbolsPrivacy)
+    // Quotes
+    app.get("quotes", "terms", use: controller.quotesTerms)
+    app.get("quotes", "privacy", use: controller.quotesPrivacy)
+    // Caro Online
+    app.get("caro", "privacy", use: controller.caroOnlinePrivacy)
+    // Fonts Keyboard
+    app.get("keyboard", "privacy", use: controller.keyboardPrivacy)
 }
